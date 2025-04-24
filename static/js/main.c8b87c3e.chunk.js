ed66a25fdaeea474aaafb481ba1171c5b564f969
naga802 <nagaskc007@gmail.com>
2025-04-24 20:08:04 +0530

    gym

--- static/js/main.c8b87c3e.chunk.js
+++ static/js/main.c8b87c3e.chunk.js
@@ -21720,7 +21720,7 @@
 					{
 						name: "BC-MB",
 						image: "/blueprint3D-assets/models/thumbnails/br-bml.jpg",
-						model: "/blueprint3D-assets/models/glb/S1.glb",
+						model: "/blueprint3D-assets/models/glb/gym1.glb",
 						type: "1",
 						stackontop: !0,
 						morph: [
