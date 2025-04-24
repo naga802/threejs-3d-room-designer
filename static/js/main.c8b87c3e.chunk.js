6ae49016b23f3b65c8aa0cce3ee9d8e716518f5f
naga802 <nagaskc007@gmail.com>
2025-04-24 16:38:02 +0530

    fin

--- static/js/main.c8b87c3e.chunk.js
+++ static/js/main.c8b87c3e.chunk.js
@@ -21720,7 +21720,7 @@
 					{
 						name: "BC-MB",
 						image: "/blueprint3D-assets/models/thumbnails/br-bml.jpg",
-						model: "/blueprint3D-assets/models/glb/BC-RC.glb",
+						model: "/blueprint3D-assets/models/glb/sbike gpt.glb",
 						type: "1",
 						stackontop: !0,
 						morph: [
